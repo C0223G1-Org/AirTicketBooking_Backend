3306efb20738f76976cab4474ad4fd6de127bb26
phuoctai0612 <sieuem2002@gmail.com>
2023-08-10 15:55:09 +0700

    tai commit third time

--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/service/customer/ICustomerService.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/service/customer/ICustomerService.java
@@ -6,11 +6,40 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 
 public interface ICustomerService {
+    /**
+     * @param pageable
+     *Create by: TàiMP
+     *Date create: 10/08/2023
+     *getListCustomer()
+     *return: Page
+     */
     Page<Customer> getListCustomer(Pageable pageable);
+    /**
+     * @param pageable,email,name,nationality
+     *Create by: TàiMP
+     *Date create: 10/08/2023
+     * Method: getListSearchCustomer()
+     * return: Page
+     */
 
     Page<Customer> getListSearchCustomer(Pageable pageable, String email, String name, String nationality);
+    /**
+     * @param id
+     * Create by: TàiMP
+     * Date create: 10/08/2023
+     * Method: deleteCustomer()
+     * return: void
+     */
 
     void deleteCustomer(Long id);
+    /**
+     * @param id
+     * Create by: TàiMP
+     * Date create: 10/08/2023
+     * Method: findCustomerById()
+     * return: Customer
+     */
+
 
     Customer findCustomerById(Long id);
 }
