257f83c3529dd5809f0a8099f1bd6c74435f859f
phuoctai0612 <sieuem2002@gmail.com>
2023-08-10 15:26:47 +0700

    tai commit first time

--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/controller/customer/CustomerController.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/controller/customer/CustomerController.java
@@ -1,4 +1,70 @@
 package com.example.air_ticket_booking.controller.customer;
 
+import com.example.air_ticket_booking.model.customer.Customer;
+import com.example.air_ticket_booking.service.customer.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+    @Autowired
+    private ICustomerService iCustomerService;
+
+    /**
+     * @param pageable
+     * @return if getListCustomer have data return getListCustomer and status OK else return status NO_CONTENT
+     *Create by: TàiMP
+     *Date create: 10/08/2023
+     */
+    @GetMapping("")
+    public ResponseEntity<Page<Customer>> getListCustomers(Pageable pageable) {
+        Page<Customer> getListCustomer = iCustomerService.getListCustomer(pageable);
+        if (getListCustomer.getTotalElements() != 0) {
+            return new ResponseEntity<>(getListCustomer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+
+    /**
+     * @param pageable, name, nationality, email
+     * @return if getListSearchCustomer have data return getListSearchCustomer and status OK else return status NO_CONTENT
+     *Create by: TàiMP
+     *Date create: 10/08/2023
+     */
+    @GetMapping("search/name={name}/nationality={nationality}/email={email}")
+    public ResponseEntity<Page<Customer>> getListSearchCustomer(Pageable pageable, @PathVariable("email") String email,
+                                                                @PathVariable("name") String name, @PathVariable("nationality") String nationality) {
+        Page<Customer> getListSearch = iCustomerService.getListSearchCustomer(pageable, email, name, nationality);
+        if (getListSearch.getTotalElements() != 0) {
+            return new ResponseEntity<>(getListSearch, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+
+    /**
+     * @param id
+     * @return if iCustomerService.findCustomerById(id) have data return status OK and set flag customer = true that customer else return status NOT_FOUND
+     *Create by: TàiMP
+     *Date create: 10/08/2023
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteCustomer(@PathVariable("id") Long id) {
+        if (iCustomerService.findCustomerById(id) != null) {
+            iCustomerService.deleteCustomer(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/service/customer/ICustomerService.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/service/customer/ICustomerService.java
@@ -1,4 +1,16 @@
 package com.example.air_ticket_booking.service.customer;
 
+import com.example.air_ticket_booking.model.customer.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+
 public interface ICustomerService {
+    Page<Customer> getListCustomer(Pageable pageable);
+
+    Page<Customer> getListSearchCustomer(Pageable pageable, String email, String name, String nationality);
+
+    void deleteCustomer(Long id);
+
+    Customer findCustomerById(Long id);
 }
