9502f6aac29cf2735b1e83d6f9e6d0ca17fd36da
Tran Thanh Son <sonva2002@gmail.com>
2023-08-10 10:43:01 +0700

    thanh son push thong tin model

--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/repository/post/IPostRepository.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/repository/post/IPostRepository.java
@@ -1,4 +1,11 @@
 package com.example.air_ticket_booking.repository.post;
 
-public interface IPostRepository {
+import com.example.air_ticket_booking.model.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPostRepository extends JpaRepository<Post,Long> {
+
+
+    Page<Post> getListPostByFlag_
 }
