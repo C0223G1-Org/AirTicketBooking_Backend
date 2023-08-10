4c24d2e4f97bd21af3e5b8ea813a44c66e98092a
Shabi96 <vuvivokiller@gmail.com>
2023-08-10 15:20:55 +0700

    thai commit lan 1

--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/controller/route/RouteController.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/controller/route/RouteController.java
@@ -1,4 +1,26 @@
 package com.example.air_ticket_booking.controller.route;
 
+import com.example.air_ticket_booking.service.route.IRouteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/route")
 public class RouteController {
+    @Autowired
+    private IRouteService routeService;
+
+    /**
+     * method dùng để lấy danh sách top 10 chuyến bay giá rẻ nhất
+     * @author ThaiVV
+     * @since 10/08/2023
+     * @return List<Route>
+     */
+    @GetMapping("/top10")
+    public ResponseEntity<?> getTop10Cheapest() {
+        return new ResponseEntity<>(routeService.findTop10RouteCheapest(), HttpStatus.OK);
+    }
 }
--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/service/route/IRouteService.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/service/route/IRouteService.java
@@ -1,4 +1,9 @@
 package com.example.air_ticket_booking.service.route;
 
+import com.example.air_ticket_booking.model.route.Route;
+
+import java.util.List;
+
 public interface IRouteService {
+    List<Route> findTop10RouteCheapest();
 }
