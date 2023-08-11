6f42c781127b033021653be0d265b1aae3284f6f
Nguyen Tuan Kiet <github.com>
2023-08-11 09:57:01 +0700

    Kiet commit code search ticket lan 2

--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/repository/ticket/ITicketRepository.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/repository/ticket/ITicketRepository.java
@@ -18,7 +18,17 @@ public interface ITicketRepository extends JpaRepository<Ticket, Long> {
      * return List Ticket
      */
     @Query(nativeQuery = true,
-            value = "select * from ticket where name_passenger like concat('%', :namePassenger, '%') and id_card_passenger = :idCardPassenger;")
+            value = " select * from ticket t\n" +
+                    "    join type_ticket tt on tt.id_type_ticket = t.type_ticket_id_type_ticket\n" +
+                    "    join luggage l on l.id_luggage = t.luggage_id_luggage\n" +
+                    "    join type_passenger tp on t.type_passenger_id_type_passenger = tp.id_type_passenger\n" +
+                    "    join seat s on t.seat_id_seat = s.id_seat\n" +
+                    "    join type_seat ts on s.id_type_seat = ts.id_type_seat\n" +
+                    "    join route r on s.id_route = r.id_route\n" +
+                    "    join air_craft ac on ac.id_air_craft = r.id_air_craft\n" +
+                    "    join destination d on d.id_destination = r.id_destination\n" +
+                    "    join departure dt on dt.id_departure = r.id_departure\n" +
+                    "    where t.name_passenger like concat('%', :namePassenger, '%') and t.id_card_passenger = :idCardPassenger;")
     List<Ticket> findTicketByNameAndIdCardPassengers(@Param("namePassenger") String namePassenger,
                                                      @Param("idCardPassenger") String idCardPassenger);
 }
