9e378a2fc5345184504248f447dfa0885206b941
tuanvu0412 <tuanvu041299@gmail.com>
2023-08-11 15:54:15 +0700

    VuDT commit test case update ticket and test case getTicket by id. 11/08/2023

--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/controller/ticket/TicketController.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/controller/ticket/TicketController.java
@@ -15,9 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -111,5 +115,17 @@ public class TicketController {
 
         iTicketService.updateTicket(id, price, flag, name, gender, email, tel, idCard, dateBooking, typeTicket, luggage, typePassenger, seat, customer);
         return ResponseEntity.ok("Cập nhật vé thành công");
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
