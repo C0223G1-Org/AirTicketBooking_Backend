d94d82e1245f4bba31c52f2e70ec818031badcb7
hunglv98 <hungle060419982@gmail.com>
2023-08-15 21:30:28 +0700

    commit chuc nang create, edit Customer lan 5 ngay 15/08

--- air_ticket_booking/src/main/java/com/example/air_ticket_booking/service/account/impl/AccountService.java
+++ air_ticket_booking/src/main/java/com/example/air_ticket_booking/service/account/impl/AccountService.java
@@ -1,6 +1,6 @@
 package com.example.air_ticket_booking.service.account.impl;
 
-import com.example.air_ticket_booking.config.JwtUserDetails;
+//import com.example.air_ticket_booking.config.JwtUserDetails;
 import com.example.air_ticket_booking.dto.account.AccountDto;
 import com.example.air_ticket_booking.model.account.Account;
 import com.example.air_ticket_booking.model.customer.Customer;
@@ -28,43 +28,44 @@ import java.util.List;
  * @return
  */
 @Service
-public class AccountService implements UserDetailsService,IAccountService {
+public class AccountService implements IAccountService {
     @Autowired
     private IAccountRepository accountRepository;
     @Autowired
     private ICustomerService customerService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Account account = accountRepository.findByUsername(username);
-        if (account == null) {
-            throw new UsernameNotFoundException("User not found with username: " + username);
-        }
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        String role = account.getRole().getNameRole();
-        authorities.add(new SimpleGrantedAuthority(role));
-
-        return new JwtUserDetails(account.getIdAccount(),account.getUsername(), account.getPassword(), authorities);
-    }
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        Account account = accountRepository.findByUsername(username);
+//        if (account == null) {
+//            throw new UsernameNotFoundException("User not found with username: " + username);
+//        }
+//        List<GrantedAuthority> authorities = new ArrayList<>();
+//        String role = account.getRole().getNameRole();
+//        authorities.add(new SimpleGrantedAuthority(role));
+//
+//        return new JwtUserDetails(account.getIdAccount(),account.getUsername(), account.getPassword(), authorities);
+//    }
 
     @Override
     public boolean signUp(AccountDto accountDto) {
-        String email = accountDto.getEmailCustomer();
-        Boolean checkExistAccount = checkExistAccount(email);
-        Boolean checkExistCustomer = checkExistCustomer(email);
-        if(checkExistAccount || checkExistCustomer){
-            return false;
-        }
-        String encoderPassword = passwordEncoder.encode(accountDto.getPassword());
-        this.accountRepository.saveAccount(accountDto.getEmailCustomer(),encoderPassword);
-        Account accountNew = accountRepository.findByUsername(email);
-        if(accountNew == null){
-            return false;
-        }
-        this.customerService.createCustomer(accountDto,accountNew.getIdAccount());
-        return checkExistAccount(email) == checkExistCustomer(email);
+//        String email = accountDto.getEmailCustomer();
+//        Boolean checkExistAccount = checkExistAccount(email);
+//        Boolean checkExistCustomer = checkExistCustomer(email);
+//        if(checkExistAccount || checkExistCustomer){
+//            return false;
+//        }
+//        String encoderPassword = passwordEncoder.encode(accountDto.getPassword());
+//        this.accountRepository.saveAccount(accountDto.getEmailCustomer(),encoderPassword);
+//        Account accountNew = accountRepository.findByUsername(email);
+//        if(accountNew == null){
+//            return false;
+//        }
+//        this.customerService.createCustomer(accountDto,accountNew.getIdAccount());
+//        return checkExistAccount(email) == checkExistCustomer(email);
+        return false;
     }
 
     /**
